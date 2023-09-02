dbbea1738aae80c3a6eeb0ea5bab1d5c3bd0791a
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-09-02 21:32:06 +0100

    Add task

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -14,3 +14,22 @@ def hash_password(password: str) -> bytes:
     """
     password = password.encode('utf-8')
     return (bcrypt.hashpw(password, bcrypt.gensalt()))
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check for password match
+
+        args:
+            hashed_password: bytes type - to be compared with the password
+            password: string type - to be checked.
+
+        return:
+            True: if the password matches
+            False: if the passwoed does not match
+    """
+    password = hash_password(password)
+
+    if password == hash_password:
+        return True
+    else:
+        return False
