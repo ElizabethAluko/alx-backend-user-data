9e5f007f7fff0c3870dcc133c3968af69e9f1e15
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-09-05 21:14:34 +0100

    Add task

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+from typing import List, TypeVar
+from flask import request
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Placeholder method for require_auth.
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Placeholder method for authorization_header.
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Placeholder method for current_user.
+        """
+        return None
