a53ccfcc69e61d1dee55e9262335292e587e5b62
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-09-06 10:02:59 +0100

    Add task

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,9 +10,33 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        Placeholder method for require_auth.
+        Returns True if the path is not in the list of strings excluded_paths.
+
+        Returns True if path is None.
+        Returns True if excluded_paths is None or empty.
+        Returns False if path is in excluded_paths.
+
+        Assumes excluded_paths contain string paths always ending with a /.
+        This method is slash tolerant.
+
+        Args:
+            path (str): The path to check for authentication.
+            excluded_paths (List[str]): List of excluded paths.
+
+        Returns:
+            bool: True if authentication is required, False otherwise.
         """
-        return False
+
+        # If path is None or excluded_paths is None/empty.
+        if path is None or not excluded_paths:
+            return True
+
+        # Ensure path ends with a slash.
+        if not path.endswith('/'):
+            path += '/'
+
+        # Check if path is in excluded_paths (slash tolerant).
+        return not any(path.startswith(ex_path) for ex_path in excluded_paths)
 
     def authorization_header(self, request=None) -> str:
         """
