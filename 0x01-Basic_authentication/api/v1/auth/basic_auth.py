1991e9ce3ecafd3a5dd84401104cb0f4c21d0db9
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-09-07 21:52:48 +0100

    Add task

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -15,8 +15,33 @@ class BasicAuth(Auth):
         if not authorization_header or not isinstance(
                 authorization_header, str):
             return None
+
         authorization_header = authorization_header.split()
+
         if len(authorization_header) != 2 or authorization_header[
                 0] != "Basic":
             return None
+
         return authorization_header[1]
+
+
+    def decode_base64_authorization_header(
+            self,base64_authorization_header: str) -> str:
+        """
+        Decode base64 authorization authentication
+        returns the decoded value of a Base64 string
+        """
+        if base64_authorization_header == None or not isinstance(
+                base64_authorization_header, str):
+            return None
+        try:
+            # Attempt to decode the Base64 string
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+
+            # Decode the bytes to a UTF-8 string
+            decoded_string = decoded_bytes.decode('utf-8')
+
+            return decoded_string
+        except base64.binascii.Error:
+            # Handle the case where decoding as Base64 fails
+            return None
