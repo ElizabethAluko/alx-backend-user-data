9a7b311b7e3df2c8b8dd60ce6a94fc406ba8f019
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-09-07 18:23:45 +0100

    Add task

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,14 +6,17 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Returns Basic authentication"""
-    
+
     def extract_base64_authorization_header(self,
-        authorization_header: str) -> str:
+                                            authorization_header: str
+                                            ) -> str:
         """Returns the base64 string of the authentication header"""
 
-        if not authorization_header or not instanceof(authorization_header, str):
+        if not authorization_header or
+        not instanceof(authorization_header, str):
             return None
         authorization_header = authorization_header.split()
-        if len(authorization_header) != 3 or authorization_header[1] != "Basic":
+        if len(authorization_header) != 3 or
+        authorization_header[1] != "Basic":
             return None
         return authorization_header[2]
