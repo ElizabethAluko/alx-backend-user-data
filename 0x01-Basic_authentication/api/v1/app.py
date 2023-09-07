74496a7bb2b788399e04900aa67b6b3a03088e8f
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-09-07 15:57:49 +0100

    Add task

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,12 +15,16 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth = getenv("AUTH_TYPE", default=None)
 
-if auth.lower() == "basic_auth":
-    from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
+
+if auth:
+    if auth.lower() == "basic_auth":
+        from api.v1.auth.basic_auth import BasicAuth
+        auth = BasicAuth()
+    else:
+        from api.v1.auth.auth import Auth
+        auth = Auth()
 else:
-    from api.v1.auth.auth import Auth
-    auth = Auth()
+    auth = None
 
 
 @app.before_request
