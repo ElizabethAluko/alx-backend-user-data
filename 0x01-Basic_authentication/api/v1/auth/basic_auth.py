aa2a3d19a2d31cb4a0f2c255e45c31b4150e2c1d
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-09-07 18:26:32 +0100

    Add task

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -12,11 +12,9 @@ class BasicAuth(Auth):
                                             ) -> str:
         """Returns the base64 string of the authentication header"""
 
-        if not authorization_header or not instanceof(
-                authorization_header, str):
+        if not authorization_header or not instanceof(authorization_header, str):
             return None
         authorization_header = authorization_header.split()
-        if len(authorization_header) != 3 or
-        authorization_header[1] != "Basic":
+        if len(authorization_header) != 3 or authorization_header[1] != "Basic":
             return None
         return authorization_header[2]
