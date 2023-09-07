501dd15a8bc3176459b5117c4eea273b24d2f8f6
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-09-07 23:39:39 +0100

    Add task

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -43,3 +43,20 @@ class BasicAuth(Auth):
         except base64.binascii.Error:
             # Handle the case where decoding as Base64 fails
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        Returns the user email and password from the Base64 decoded
+        value.
+        """
+        if decoded_base64_authorization_header is None or not isinstance(
+                decoded_base64_authorization_header, str):
+            return (None, None)
+
+        if ':' not in decoded_base64_authorization_header:
+            return (None, None)
+
+        user_email, user_password =
+        decoded_base64_authorization_header.split(':', 1)
+        return (user_email, user_password)
