7e350c4ffb7aff3d108e63a222a623c8bd0334d6
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-09-08 11:53:19 +0100

    Add task

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -60,3 +60,28 @@ class BasicAuth(Auth):
         user_email, user_password = decoded_base64_authorization_header.split(
                 ':', 1)
         return (user_email, user_password)
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """
+        returns the User instance based on his email and password
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        # Search for users with the given email in the database
+        users_with_email = User.search(user_email)
+
+        # Check if there are users with the provided email
+        if not users_with_email:
+            return None
+
+        # Iterate through users with the given email
+        for user in users_with_email:
+            # Check if the provided password is valid for this user
+            if user.is_valid_password(user_pwd):
+                return user
+
+        # If no matching user with the provided password, return None
+        return None
