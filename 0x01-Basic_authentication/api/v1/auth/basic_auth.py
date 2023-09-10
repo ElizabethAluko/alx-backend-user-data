c8aa01d8a03f450d9d85e34d7a6cb403b61f861b
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-09-10 15:30:57 +0100

    Add task

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -78,9 +78,9 @@ class BasicAuth(Auth):
         if not users:
             return None
 
-        user = users[0]
-        if user.is_valid_password(user_pwd):
-            return user
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
 
         return None
 
