b63868d5b2979ffc5fb79cceb25d79276bf7a6f0
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-09-12 15:38:53 +0100

    Add task

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+
 
 from user import Base, User
 
@@ -40,8 +43,33 @@ class DB:
         Returns:
             User: The User object that was added to the database.
         """
-        new_user = User(email=email, hashed_password=hashed_password)
+        new_user = User()
+        new_user.email = email
+        new_user.hashed_password = hashed_password
         self._session.add(new_user)
         self._session.commit()
 
         return new_user
+
+    def find_user_by(self, **kwargs):
+        """Find and return the first user in the users table based
+        on input arguments.
+
+        Args:
+            **kwargs: Arbitrary keyword arguments for filtering.
+
+        Returns:
+            User: The first User object that matches the filter criteria.
+
+        Raises:
+            NoResultFound: If no matching user is found.
+            InvalidRequestError: If the query arguments are invalid.
+        """
+
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound('Not found')
+            return user
+        except InvalidRequestError as e:
+            raise e
