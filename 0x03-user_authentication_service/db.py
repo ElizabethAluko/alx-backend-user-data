d64b4b5e2da5f791ee71844774c75db84cb9e208
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-09-12 17:55:05 +0100

    Add task

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.exc import InvalidRequestError, NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 
 from user import Base, User
@@ -43,9 +43,9 @@ class DB:
         Returns:
             User: The User object that was added to the database.
         """
-        new_user = User()
-        new_user.email = email
-        new_user.hashed_password = hashed_password
+        new_user = User(
+                email = email,
+                hashed_password = hashed_password)
         self._session.add(new_user)
         self._session.commit()
 
