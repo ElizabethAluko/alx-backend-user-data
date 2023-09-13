ce4d56564abd706f9637ee41e80aa62428b7aaa7
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-09-14 00:49:24 +0100

    Add task

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
@@ -63,3 +64,7 @@ class Auth:
             pass
 
         return False
+
+    def _generate_uuid(self) -> str:
+        """Generate a new uuid and return it as a string"""
+        return str(uuid.uuid4())
