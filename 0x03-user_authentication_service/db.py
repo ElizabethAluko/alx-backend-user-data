d79fd050427d4ecf79d915a162603e985f5a37b7
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-09-13 22:48:59 +0100

    Add task

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -64,7 +64,7 @@ class DB:
         try:
             user = self._session.query(User).filter_by(**kwargs).first()
             if user is None:
-                raise NoResultFound('Not found')
+                raise NoResultFound()
             return user
         except InvalidRequestError as e:
             raise e
