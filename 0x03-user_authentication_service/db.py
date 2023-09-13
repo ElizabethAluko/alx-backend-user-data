b68b91d342c0757b00a04b55b9b7b71d17ac400a
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-09-13 17:43:39 +0100

    Add task

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -7,11 +7,10 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
+from user import Base, User
 import logging
 
 logging.basicConfig(level=logging.ERROR)
-
-from user import Base, User
 
 
 class DB:
