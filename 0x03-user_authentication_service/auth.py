290d6b60e63ec0a2bc02a339134a38b235bcdede
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-09-13 12:56:57 +0100

    Add task

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,8 +3,8 @@
 
 
 def _hash_password(self, password: str) -> bytes:
-            """Hash Password"""
-            salt = bcrypt.gensalt()
+    """Hash Password"""
+    salt = bcrypt.gensalt()
 
-            hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-            return hashed_password
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
