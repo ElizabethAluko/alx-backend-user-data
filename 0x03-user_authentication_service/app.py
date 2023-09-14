65bf818c1e1405411f17dcfa792af5c2b6df4695
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-09-14 17:29:00 +0100

    Add task

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -85,10 +85,10 @@ def profile():
         user = AUTH.get_user_from_session_id(session_id)
 
         if user:
-            return make_response(jsonify({"email": user.email}), 200)
+            return jsonify({"email": user.email}), 200
 
         else:
-            abort(403)
+            return 403
 
 
 if __name__ == "__main__":
