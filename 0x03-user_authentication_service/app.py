3537baee2dae9a5ebf7089878c8a2381c314a628
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-09-14 13:35:18 +0100

    Add task

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -59,7 +59,7 @@ def logout():
     """Delete stored session_id to logout the user"""
 
     if request.method == 'DELETE':
-        session_id = request.cookies.get('session_id')
+        session_id = request.cookie.get('session_id')
 
     # Find the user with the session id.
     user = AUTH.find_user_by_session_id(session_id)
