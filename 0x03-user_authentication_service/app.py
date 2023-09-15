299f6d7db14452e678fc806ee4069b5b8fd62fc0
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-09-16 00:35:19 +0100

    Add task

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -76,16 +76,14 @@ def logout():
 @app.route('/profile', methods=['GET'])
 def profile():
     """Send the user profile kn GET request"""
-    if request.method == 'GET':
-        session_id = request.cookies.get('session_id')
+    session_id = request.cookies.get('session_id')
+    if not session_id:
+        abort(403)
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
 
-        user = AUTH.get_user_from_session_id(session_id)
-
-        if user:
-            return jsonify({"email": user.email}), 200
-
-        elif not user or session_id == '':
-            return "Forbidden", 403
+    return jsonify({"email": user.email}), 200
 
 
 @app.route('/reset_password', methods=['POST'])
