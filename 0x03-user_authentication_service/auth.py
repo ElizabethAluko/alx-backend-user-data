8a7c53f8cd357c8ceadb42661d1e01821b2308c4
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-09-15 22:56:51 +0100

    Add task

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -109,20 +109,24 @@ class Auth:
     def get_reset_password_token(self, email: str) -> str:
         """Get a password reset token of a user with the given email"""
 
-        user = self._db.find_user_by(email=email)
-        if not user:
-            raise ValueError("User does not exist")
+        try:
+            user = self._db.find_user_by(email=email)
 
-        reset_token = _generate_uuid()
-        self._db.update_user(user.id, reset_token=reset_token)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
 
-        return reset_token
+            return reset_token
+
+        except NoResultFound:
+            raise ValueError
 
     def update_password(self, reset_token: str, password: str) -> None:
         """Update the new password for the user"""
-        user = self._db.find_user_by(reset_token=reset_token)
-        if not user:
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = _hash_password(password)
+            self._db.update_user(user.id,
+                                 hashed_password=hashed_password,
+                                 reset_token=None)
+        except NoResultFound:
             raise ValueError
-        hashed_password = _hash_password(password)
-        self._db.update_user(user.id, hashed_password=hashed_password,
-                             reset_token=None)
