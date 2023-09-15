636ac476ae3028cbcd10dee7d25e9e7c30002730
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-09-15 05:14:26 +0100

    Add task

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -104,3 +104,16 @@ class Auth:
         """Destroy a user session with the given user_id"""
         self._db.update_user(user_id, session_id=None)
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Get a password reset token of a user with the given email"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                reset_token = self._generate_uuid()
+                self._db.update_user(user.id, reset_token=reset_token)
+
+                return reset_token
+
+        except ValueError as e:
+            return e
