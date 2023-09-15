9ce0b3b0e5ee3467baf0b8aea7895d4e9ca5eac4
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-09-15 05:25:19 +0100

    Add task

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -108,13 +108,11 @@ class Auth:
     def get_reset_password_token(self, email: str) -> str:
         """Get a password reset token of a user with the given email"""
 
-        try:
-            user = self._db.find_user_by(email=email)
-            if user:
-                reset_token = self._generate_uuid()
-                self._db.update_user(user.id, reset_token=reset_token)
+        user = self._db.find_user_by(email=email)
+        if not user:
+            raise ValueError
 
-                return reset_token
+        reset_token = self._generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
 
-        except ValueError as e:
-            raise e
+        return reset_token
