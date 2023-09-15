bf976b01b3dfcfb6a5d3227fa9917c77b71d85dd
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-09-15 18:43:38 +0100

    Add task

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -94,9 +94,8 @@ def get_reset_password_token():
     if request.method == 'POST':
         email = request.form.get('email')
         reset_password_token = AUTH.get_reset_password_token(email)
-        if reset_password == None:
-            return "user doest not exist"
-        elif not reset_password_token:
+
+        if not reset_password_token:
             abort(403)
         else:
             response = jsonify({"email": "<user email>",
