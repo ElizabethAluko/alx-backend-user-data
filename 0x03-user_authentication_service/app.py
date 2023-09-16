62e9d9dc6e2b63900e49ac3403618033c3e68d28
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-09-16 17:36:48 +0100

    Add task

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -70,7 +70,7 @@ def logout():
         AUTH.destroy_session(user.user_id)
 
         # Redirect to GET /
-        return  make_response(redirect('/')), 302
+        return redirect('/')
 
     except ValueError:
         abort(403)
